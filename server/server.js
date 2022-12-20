7ad98663e66ce0d2b753cef5ca3e286d578b49c0
AnthonyDampier <Dampieranthony2@gmail.com>
2022-12-20 10:55:55 -0600

    workout type & exercise routers created

--- server/server.js
+++ server/server.js
@@ -9,6 +9,8 @@ const passport = require('./strategies/user.strategy');
 
 // Route includes
 const userRouter = require('./routes/user.router');
+const workoutGenreRouter = require('./routes/workout_genre.router');
+const exerciseTypeRouter = require('./routes/exercise_types.router')
 
 // Body parser middleware
 app.use(bodyParser.json());
@@ -23,6 +25,8 @@ app.use(passport.session());
 
 /* Routes */
 app.use('/api/user', userRouter);
+app.use('./api/workoutGenre', workoutGenreRouter);
+app.use('./api/exerciseType', exerciseTypeRouter);
 
 // Serve static files
 app.use(express.static('build'));
