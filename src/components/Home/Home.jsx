cba9e73936ee59edb8abd04f43560eb15ca0fc72
AnthonyDampier <Dampieranthony2@gmail.com>
2023-01-06 12:59:36 -0600

    login experience fixed

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -3,22 +3,28 @@ import LogOutButton from '../LogOutButton/LogOutButton';
 import {useSelector} from 'react-redux';
 //import jsx
 import YourTopPrograms from '../YourTopPrograms/YourTopPrograms';
-import ListOfTenPrograms from '../ListOfProgramsTable/ListOfPrograms.jsx';
+import ListOfTwentyPrograms from '../ListOfProgramsTable/ListOfPrograms.jsx';
 
 //import css
-import './UserPage.css'
+import './Home.css'
 // import CreateProgramPage from '../CreateProgram/CreateProgramPage';
 
 function UserPage() {
   // this component doesn't do much to start, just renders some user reducer info to the DOM
   const user = useSelector((store) => store.user);
+  
   return (
     <div>
       {/* <h1>Welcome, {user.username}!</h1> */}
       {/* <p>Your ID is: {user.id}</p> */}
       <div className="user-page">
-          <ListOfTenPrograms/>
-          <YourTopPrograms/>
+          <ListOfTwentyPrograms/>
+          {user.id && (
+            <>
+              <YourTopPrograms/>
+            </>
+            )
+          }
       </div>
       {/* <LogOutButton className="btn" /> */}
       {/* <div>
