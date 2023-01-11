f2fbd768b163596b8510ed770c90174903d19577
AnthonyDampier <Dampieranthony2@gmail.com>
2023-01-11 09:24:57 -0600

    completed authentication and removed needless comm

--- server/routes/workout_genre.router.js
+++ server/routes/workout_genre.router.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const pool = require('../modules/pool');
 const router = express.Router();
+const {
+  rejectUnauthenticated,
+} = require('../modules/authentication-middleware');
 
 /**
  * GET route template
  */
-router.get('/', (req, res) => {
+router.get('/', rejectUnauthenticated, (req, res) => {
   // GET route code here
     console.log('in workout_genre.router');
     const queryText = `Select * from "workout_genre";`;
@@ -24,7 +27,7 @@ router.get('/', (req, res) => {
 /**
  * POST route template
  */
-router.post('/', (req, res) => {
+router.post('/',  rejectUnauthenticated,(req, res) => {
   // POST route code here
 });
 
