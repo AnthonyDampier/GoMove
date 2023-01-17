38af83dda985b56820607e22dcb77c11e0bc6c68
AnthonyDampier <Dampieranthony2@gmail.com>
2023-01-17 15:05:15 -0600

    sql tables

--- database.sql
+++ database.sql
@@ -8,3 +8,37 @@ CREATE TABLE "user" (
     "username" VARCHAR (80) UNIQUE NOT NULL,
     "password" VARCHAR (1000) NOT NULL
 );
+
+CREATE TABLE `exercise_type` (
+	`id` INT NOT NULL AUTO_INCREMENT,
+	`exercise_name` VARCHAR(255),
+	`major_muscle_group` TEXT,
+	`targeted_muscle_group` TEXT,
+	UNIQUE KEY `exercise_pk` (`id`) USING BTREE
+);
+
+CREATE TABLE `workout_genre` (
+	`id` INT NOT NULL AUTO_INCREMENT,
+	`genre` VARCHAR(255)
+);
+
+CREATE TABLE `program_set` (
+	`program_id` INT,
+	`session_id` INT,
+	`exercise_id` INT,
+	`set_id` INT,
+	`reps` INT,
+	`percent_of_max` DECIMAL
+);
+
+CREATE TABLE `program` (
+	`id` INT NOT NULL AUTO_INCREMENT,
+	`program_title` VARCHAR(250),
+	`author_user_id` INT,
+	`program_genre` INT,
+	`num_of_sessions` INT,
+	`approved` BOOLEAN DEFAULT 'false',
+	`image` VARCHAR(400),
+	`description` VARCHAR(400)
+);
+
